0b43b79be156c33be5b61af2d8810296aa3bd7e7
DaimenGGer <daimenkg@gmail.com>
2023-11-27 00:49:55 +0600

    v5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await axios.get("/api/todos");
+    setTodos(response.data);
+  };
+
+  const addTodo = async () => {
+    const response = await axios.post("/api/todos", {
+      task: newTodo,
+      completed: false,
+    });
+    setTodos([...todos, response.data]);
+    setNewTodo("");
+  };
+
+  const updateTodo = async (id, completed) => {
+    const response = await axios.put(`/api/todos/${id}`, {
+      completed: !completed,
+    });
+    setTodos(todos.map((todo) => (todo._id === id ? response.data : todo)));
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`/api/todos/${id}`);
+    setTodos(todos.filter((todo) => todo._id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => updateTodo(todo._id, todo.completed)}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.task}
+            </span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
